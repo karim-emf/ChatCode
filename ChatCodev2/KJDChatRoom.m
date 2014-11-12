4b09fe4da621f6c7214b5f0f9d2cd8364a14ff02
Danny Wu <dan.wu.87@gmail.com>
2014-11-12 14:53:58 -0500

    Initial Commit

---
+++ ChatCodev2/KJDChatRoom.m
@@ -0,0 +1,13 @@
+//
+//  KJDChatRoom.m
+//  ChatCodev2
+//
+//  Created by DANNY WU on 11/12/14.
+//  Copyright (c) 2014 DANNY WU. All rights reserved.
+//
+
+#import "KJDChatRoom.h"
+
+@implementation KJDChatRoom
+
+@end
